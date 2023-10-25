f7b04a28a704350df479f3f05723baf56349206c
Marek Serafin <marek@snowheads.pl>
2023-10-25 22:16:28 +0200

    fix: start scan in the uart example after state changed to poweredOn

--- examples/uart-bind-params.js
+++ examples/uart-bind-params.js
@@ -5,13 +5,24 @@ const noble = require('../with-custom-binding')
 const noble_uart_a = noble ({ bindParams: { uart: { port: "/dev/tty.usbmodem1..." } } } )
 const noble_uart_b = noble ({ bindParams: { uart: { port: "/dev/tty.usbmodem2..." } } } )
 
-noble_uart_a.on('discover', (peripheral) => {
+noble_uart_a.on('discover', peripheral => {
     console.log("UART A", peripheral.address)
 })
 
-noble_uart_b.on('discover', (peripheral) => {
+noble_uart_b.on('discover', peripheral => {
     console.log("UART B", peripheral.address)
 })
 
-noble_uart_a.startScanning()
-noble_uart_b.startScanning()
+noble_uart_a.on('stateChange', state => {
+    if(state === 'poweredOn') {
+        noble_uart_a.setAddress("00:11:22:33:44:01");
+        noble_uart_a.startScanning()        
+    }
+});
+
+noble_uart_b.on('stateChange', state => {
+    if(state === 'poweredOn') {
+        noble_uart_b.setAddress("00:11:22:33:44:02");
+        noble_uart_b.startScanning()        
+    }
+});
